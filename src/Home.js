1076ed95a818148e5cb15546039ed6e287f76bd0
Michael0814772 <michael232akin@gmail.com>
2022-02-11 15:20:28 +0100

    first commit

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/signup" className='lg:text-7xl md:text-4xl text-2xl font-extrabold text-red-800'>Click me to continue</Link>
         </div>
     )
 }
